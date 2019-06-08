b40bae41ad1be0fec888021570bdbd61ed7aad7b
Tanya Swift <tanyanicolerussi@gmail.com>
2019-06-07 19:07:51 -0600

    Started Integrating Jasmine

---
+++ server/spec/integration/static_spec.js
@@ -0,0 +1,13 @@
+const request = require("request");
+const base = "http://localhost:8000";
+
+// describe("routes : static", () => {
+//     describe("GET /", () => {
+//         it("should return status code 200", (done) => {
+//             request.get(base, (err, res, body) => {
+//                 expect(res.statusCode).toBe(200);
+//                 done();
+//             });
+//         });
+//     });
+// });
